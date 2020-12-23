ef47d8c0949eb7c6f319bfd715347ac47aeb6d70
KAB <>
2020-12-23 10:16:39 +0000

    Write and pass tests for part 1 and 2
    Write method to find two numbers that add up to 2020 then multiply them.
    Write method to find three numbers that add up to 2020 then multiply them.
    Please enter the commit message for your changes. Lines starting

---
+++ Day01/lib/part1.rb
@@ -0,0 +1,14 @@
+# Find the two entries in the list of expenses that sum to 2020
+# and then multiply those two numbers together
+
+def find_sum_2020_multiplied(entries)
+  entries_array = entries.gsub("\n",", ").split(", ").map(&:to_i)
+  result = entries_array.combination(2).find_all { |a, b| a + b == 2020 }
+  result.flatten.inject(:*)
+end
+
+def find_three_sum_2020_multiplied(entries)
+  entries_array = entries.gsub("\n",", ").split(", ").map(&:to_i)
+  result = entries_array.combination(3).find_all { |a, b, c| a + b + c == 2020 }
+  result.flatten.inject(:*)
+end
